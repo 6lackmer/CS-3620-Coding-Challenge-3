098c58a6f67dbe3b405d16381800c29f5f849019
6lackmer <94334012+6lackmer@users.noreply.github.com>
2023-01-13 16:02:32 -0700

    Completed parts 1-3

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 # Part 1:
+    # Assumption:
+        # All students get 10% off
+        # All students are regular buyers
+        # Regular buyers all get 5% off
 def student_discount(x):
     # Student discount always gets buyer discount. Calculate student discount then buyer discount off of that rate.
     return regular_buyer_discount(x * 0.9)
@@ -9,10 +13,16 @@ def regular_buyer_discount(x):
 print(student_discount(100))
 print(regular_buyer_discount(100))
 
-
 # Part 2:
+result = (lambda x: x * (x+5) **2)(5)
+print(result)
 
 # Part 3:
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+def discount_products_10_percent(x):
+    return x * 0.9
+result = list(map(discount_products_10_percent, my_list))
+print(result)
 
 # Part 4:
 
